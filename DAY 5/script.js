bd190f72f906a959f71403d9c9a042ae67302371
mikaRamadhan <mika.ramadhan8@gmail.com>
2024-10-17 09:21:35 +0700

    js

---
+++ DAY 5/script.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML = "Belajar InnerHTML"
+document.write("Hello World!!");
+document.write("ini adalah hasil dari Document.write JS");
+window.alert("HALLO BANG AFRAN");
+
+console.log("ini adalah hasil console log");
+console.info("ini adalah hasil console info");
+console.warn("ini adalah hasil console warning");
+console.error("ini adalah hasil console Error");
+
+const dataTable = [
+    {"id" : 1, "name" : "andre"},
+    {"id" : 2, "name" : "afran"}
+];
+console.table(dataTable);
